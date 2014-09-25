23467aa5d8530546bbc660d9bced5301ba0a5836
Kedan Li <kedanli@KedantekiMacBook-Pro.local>
2014-09-25 00:48:01 -0500

    Initial Commit

---
+++ NEWSATHacker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NEWSATHacker
+//
+//  Created by Kedan Li on 14-9-25.
+//  Copyright (c) 2014年 Kedan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
