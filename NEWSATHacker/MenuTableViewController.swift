8316243ecd43bbb86246cc5070f4b98cbbea4788
yuwang17 <yuwang17@illinois.edu>
2014-11-04 19:27:55 -0600

    lalala

--- NEWSATHacker/MenuTableViewController.swift
+++ NEWSATHacker/MenuTableViewController.swift
@@ -8,28 +8,54 @@
 
 import UIKit
 
-class MenuTableViewController: UIViewController {
-
+class MenuTableViewController: UIViewController, UIScrollViewDelegate{
+    
+    @IBOutlet var scroller : UIScrollView!
+    @IBOutlet var settings : UIButton!
+    @IBOutlet var account : UIButton!
+    @IBOutlet var ranking : UIButton!
+    @IBOutlet var buyTests : UIButton!
+    @IBOutlet var history : UIButton!
+    @IBOutlet var aboutUs : UIButton!
+    @IBOutlet var guidance : UIButton!
+    @IBOutlet var friends : UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        scroller.contentSize = CGSizeMake(200, 600)
+        scroller.showsVerticalScrollIndicator = false
+        
+        var itemBack = UIImageView(frame: CGRectMake(0, 0, 200, 40))
+        itemBack.alpha = 0.7
+        
+        //itemBack.frame = UIColor(red: 111.0/255, green: 111.0/255, blue: 111.0/255, alpha: 1)
+        
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
